f934ab068a792c0950477e48b0cad90cc55fbaf5
Vitor <vitor.nunes@estudante.ifms.edu.br>
2022-09-09 12:36:22 -0300

    inúcio do projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+ 
+ 
+function App() {
+return(
+ 
+<form action="cadastro.html" method="post">
+  <h3>Crie sua Conta</h3>
+  <label>Nome:</label>
+  <input type="text" name="nome"></input>
+</form>
+ 
+)
+};
+ 
+export default App
